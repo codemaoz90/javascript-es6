8b3e8f4c64fbc0f2ff7e9dfa6fd9ac5b4b3fdf80
codemaoz90 <codemaoz@gmail.com>
2021-02-12 15:46:15 +0100

    Nivel 2 culminado con interfaz grafica

--- nivel2/ejercicio7.js
+++ nivel2/ejercicio7.js
@@ -2,13 +2,24 @@ window.onload = () => {
 	const tablaMultiplicar = () => {
 		const n1 = document.getElementById("n1").value,
 			n2 = document.getElementById("n2").value,
-			table = document.getElementById("table");
+			data = document.getElementById("data");
+		table = `<table class="table table-dark table-hover">
+    <thead>
+    <tr style="font-size:20px; text-align:center">
+      <th>Tabla de multiplicar</th>
+    </tr>
+    </thead>
+    `;
 		for (let index = 1; index <= n2; index++) {
 			//console.log(`${n1} * ${index} = ${n1 * index}`);
-			let tr = document.createElement("tr");
-
-			let td = document.createElement("td");
+			table += `<tr style="font-size:20px; text-align:center">
+      <td>${n1} x ${index} = ${n1 * index}</td>
+      </tr>
+      
+      `;
+			data.innerHTML = table;
 		}
+		table += "</table>";
 	};
 
 	document.getElementById("btn").addEventListener("click", tablaMultiplicar);
